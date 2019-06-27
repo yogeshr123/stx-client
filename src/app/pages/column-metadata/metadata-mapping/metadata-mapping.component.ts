0707234a70cd97989c8b1bbf554eec28114cf3a2
Yogesh Rathod <yogesh@mactores.com>
2019-06-27 11:37:17 +0530

    metadata mapping ui

---
+++ src/app/pages/column-metadata/metadata-mapping/metadata-mapping.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-metadata-mapping',
+  templateUrl: './metadata-mapping.component.html',
+  styleUrls: ['./metadata-mapping.component.css']
+})
+export class MetadataMappingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

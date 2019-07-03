79bb806f90f18d7558cd0df685e133c100f852f6
Yogesh Rathod <yogesh@mactores.com>
2019-07-03 11:59:24 +0530

    CMV State mgmt

--- src/app/pages/column-metadata/column-metadata.component.ts
+++ src/app/pages/column-metadata/column-metadata.component.ts
@@ -33,12 +33,16 @@ export class ColumnMetadataComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.state = this.commonService.getState();
-    if (this.state.version) {
-      this.viewData(this.state.version);
-    }
     this.getVersions();
     this.getAllTables();
+    this.state = this.commonService.getState();
+  }
+
+  checkStateUpdateSelectedTable() {
+    if (this.state.version) {
+      const selectedVersion = this.versions.filter(i => i.METADATA_VERSION === this.state.version);
+      this.viewData(selectedVersion[0]);
+    }
   }
 
   getAllTables() {
@@ -62,6 +66,7 @@ export class ColumnMetadataComponent implements OnInit {
     this.columnMetadataService.getTableVersions(request).subscribe((resp: any) => {
       this.versions = resp.data;
       this.loader.versions = false;
+      this.checkStateUpdateSelectedTable();
       this.versions.forEach(element => {
         if (element.STATUS.toLowerCase() === 'new') {
           this.showGenerateVersion = false;
@@ -73,10 +78,9 @@ export class ColumnMetadataComponent implements OnInit {
   }
 
   viewData(version) {
-    console.log("version ", version);
-    this.state.version = version.METADATA_VERSION || version;
+    this.state.version = version.METADATA_VERSION;
     this.commonService.setState(this.state);
-    this.selectedVersion = version.METADATA_VERSION || version;
+    this.selectedVersion = version;
     this.loader.columns = true;
     const request = {
       table_name: this.selectedTable,

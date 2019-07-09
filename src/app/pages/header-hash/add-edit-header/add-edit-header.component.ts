867898d2fc99acadf90d41638c391f5c038aee76
Yogesh Rathod <yogesh@mactores.com>
2019-07-09 19:06:19 +0530

    Add edit header in ui

--- src/app/pages/header-hash/add-edit-header/add-edit-header.component.ts
+++ src/app/pages/header-hash/add-edit-header/add-edit-header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-add-edit-header',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditHeaderComponent implements OnInit {
 
-  constructor() { }
+  addEditHeaderForm: FormGroup;
+  routeInfo = {
+    path: '',
+    id: '',
+    isViewOnly: false,
+    isEditMode: false
+  };
+  TABLE_NAME: any;
+  appState: any;
+
+  constructor(
+    private messageService: MessageService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private formBuilder: FormBuilder
+  ) {
+    this.route.params.subscribe(params => {
+      this.routeInfo.id = params.headerId;
+    });
+    this.route.url.subscribe(params => {
+      this.routeInfo.path = params[0].path;
+      if (this.routeInfo.path.indexOf('view') > -1) {
+        this.routeInfo.isViewOnly = true;
+      }
+      if (this.routeInfo.path.indexOf('edit') > -1) {
+        this.routeInfo.isEditMode = true;
+      }
+    });
+  }
 
   ngOnInit() {
+  }
+
+  goBack() {
+    this.location.back();
   }
 
 }

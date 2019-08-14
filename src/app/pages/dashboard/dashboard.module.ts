e3c78ba827f485fe36e7c42da6c5358f307976f9
Yogesh Rathod <yogesh@mactores.com>
2019-08-14 13:09:43 +0530

    Dasboard Routes in seperate file

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 // NgBootstrap
+import { NgxLoadingModule } from 'ngx-loading';
 import { DashboardComponent } from './dashboard.component';
 import { TableModule } from 'primeng/table';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { MessageService } from 'primeng/api';
 import { DataLatencySummaryComponent } from './data-latency-summary/data-latency-summary.component';
 import { LoadingStatusSummaryComponent } from './loading-status-summary/loading-status-summary.component';
+import { DashboardRouting } from './dashboard.routing';
 
 
 @NgModule({
@@ -19,20 +21,8 @@ import { LoadingStatusSummaryComponent } from './loading-status-summary/loading-
         FormsModule,
         TableModule,
         MultiSelectModule,
-        RouterModule.forChild([
-            {
-                path: '',
-                component: DashboardComponent
-            },
-            {
-                path: 'data-latency-summary',
-                component: DataLatencySummaryComponent
-            },
-            {
-                path: 'loading-status-summary',
-                component: LoadingStatusSummaryComponent
-            }
-        ]),
+        DashboardRouting,
+        NgxLoadingModule.forRoot({}),
     ],
     providers: [MessageService],
     declarations: [
---
+++ src/app/pages/dashboard/dashboard.routing.ts
@@ -0,0 +1,27 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { DashboardComponent } from './dashboard.component';
+import { DataLatencySummaryComponent } from './data-latency-summary/data-latency-summary.component';
+import { LoadingStatusSummaryComponent } from './loading-status-summary/loading-status-summary.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: DashboardComponent
+    },
+    {
+        path: 'data-latency-summary',
+        component: DataLatencySummaryComponent
+    },
+    {
+        path: 'loading-status-summary',
+        component: LoadingStatusSummaryComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class DashboardRouting { }

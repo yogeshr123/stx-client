d30c799d1ad88998a7ad0791c99b0845ab07fe4d
varsha <varsha@mactores.com>
2019-08-14 17:07:46 +0530

    sidebar menu expand collapse added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   baseUrl: 'http://10.219.42.183:8080/api/v1/',
   RAW_FACTORY_PATH_DEFAULT_SETTING: 'stx-usw2-ehc-prd-data-factory',
   T1_PATH_DEFAULT_SETTING: 'stx-usw2-ehc-prd-data-t1',
-  appModulesList: ['Load Control', 'Load Status', 'Column Metadata', 'User Management'],
+  appModulesList: ['Dashboard', 'Load Control', 'Load Status', 'Column Metadata', 'Header Hash', 'User Management', 'DB Endponits', 'Clusters'],
   defaultPermissions: ['Read', 'Add', 'Edit', 'Delete']
 };

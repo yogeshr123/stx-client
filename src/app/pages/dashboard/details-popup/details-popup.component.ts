e7e99fb733b89ca1ee7a4ec17b1afd885f7797f5
Yogesh Rathod <yogesh@mactores.com>
2019-08-16 19:03:06 +0530

    Dashboard Table Details UI

--- src/app/pages/dashboard/details-popup/details-popup.component.ts
+++ src/app/pages/dashboard/details-popup/details-popup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DynamicDialogConfig } from 'primeng/api';
+import { DashboardService } from 'src/app/services/dashboard.service';
 
 @Component({
   selector: 'app-details-popup',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPopupComponent implements OnInit {
 
-  constructor() { }
+  data: any;
+  details: any;
+  constructor(
+    private dashboardService: DashboardService,
+    public config: DynamicDialogConfig
+  ) { }
 
   ngOnInit() {
+    this.data = this.config.data;
+    this.getDetails();
+  }
+
+  getDetails() {
+    this.dashboardService.getLoadControlDetails(this.data).subscribe((resp: any) => {
+      if (resp && !resp.error) {
+        this.details = resp.data;
+        console.log("resp.data ", resp.data);
+      }
+    });
   }
 
 }

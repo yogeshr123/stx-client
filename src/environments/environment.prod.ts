b9888e6e80611dcdeb0d3ccc35908c6ad8fdba7b
Yogesh Rathod <yogesh@mactores.com>
2019-09-05 12:09:39 +0530

    ENV Url Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://10.219.42.183:8080/api/v1/',
+  baseUrl: 'http://ehcload.seagate.com:8080/api/v1/',
   RAW_FACTORY_PATH_DEFAULT_SETTING: 'stx-usw2-ehc-prd-data-factory',
   T1_PATH_DEFAULT_SETTING: 'stx-usw2-ehc-prd-data-t1',
   appModulesList: ['Dashboard', 'Load Control', 'Load Status', 'Column Metadata', 'Header Hash', 'User Management', 'DB Endponits', 'Clusters', 'Spark Config'],

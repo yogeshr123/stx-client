aa7a6e8185635f1941b417bbc1f6fdb7c49768c4
varsha <varsha@mactores.com>
2019-09-13 14:33:03 +0530

    register redirection to login issue resolved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { CommonService } from './services/common.service';
-import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +8,13 @@ import { isNullOrUndefined } from 'util';
 })
 export class AppComponent implements OnInit {
   title = 'stx-clientUI';
-  appState: any;
 
   constructor(
     private router: Router,
-    private commonService: CommonService
   ) {
   }
 
   ngOnInit() {
-    this.appState = this.commonService.getState();
-    if (isNullOrUndefined(this.appState.loggedInUser)) {
-      this.router.navigateByUrl('/superlogin');
-      return false;
-    }
-
     this.router.events.subscribe((evt) => {
       if (!(evt instanceof NavigationEnd)) {
         return;

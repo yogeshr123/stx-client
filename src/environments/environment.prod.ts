b591ba0674cfd338ab2e8d02eb3d5fc434e70a23
Yogesh Rathod <yogesh@mactores.com>
2019-10-09 13:35:44 +0530

    Updated PRD API endpoint after reverse proxy config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://ehcload.seagate.com:8443/api/v1/',
+  baseUrl: 'https://ehcload.seagate.com/api/v1/',
   RAW_FACTORY_PATH_DEFAULT_SETTING: 'stx-usw2-ehc-prd-data-factory',
   T1_PATH_DEFAULT_SETTING: 'stx-usw2-ehc-prd-data-t1',
   appModulesList: ['Dashboard', 'Load Control', 'Load Status', 'Column Metadata', 'Header Hash', 'User Management', 'DB Endponits', 'Clusters', 'Spark Config'],

21b16820b5cf67654a8aabcd55e65681fcc74daf
Yogesh Rathod <yogesh@mactores.com>
2019-10-15 14:40:56 +0530

    EMR Table Status Column Sequence Updated

--- src/app/pages/dashboard/tableColumns.ts
+++ src/app/pages/dashboard/tableColumns.ts
@@ -2,8 +2,9 @@ const EMRTableLoadingStatus = [
     { field: 'SCHEMA_NAME', header: 'SCHEMA_NAME' },
     { field: 'TABLE_NAME', header: 'TABLE_NAME' },
     { field: 'LOAD_STRATEGY', header: 'LOAD_STRATEGY' },
-    { field: 'T1_PROCESS_END_DATE', header: 'T1_PROCESS_END_DATE', type: 'date' },
-    { field: 'T2_PROCESS_END_DATE', header: 'T2_PROCESS_END_DATE', type: 'date' },
+    { field: 'TABLE_STATUS', header: 'TABLE_STATUS' },
+    { field: 'T1_MAX_LOAD_END_DATE', header: 'T1_MAX_LOAD_END_DATE', type: 'date' },
+    { field: 'T2_MAX_LOAD_END_DATE', header: 'T2_MAX_LOAD_END_DATE', type: 'date' },
     // { field: 'AS_OF', header: 'AS_OF', type: 'dateNow' }
 ];
 

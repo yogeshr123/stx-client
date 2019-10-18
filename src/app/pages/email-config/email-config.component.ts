47267f4b6956ad68335ad228398db8c6c289ecd2
Yogesh Rathod <yogesh@mactores.com>
2019-10-18 19:58:13 +0530

    Email List UI

--- src/app/pages/email-config/email-config.component.ts
+++ src/app/pages/email-config/email-config.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { DBEndpointsService } from 'src/app/services/db-endpoints.service';
+import { MessageService, DialogService } from 'primeng/api';
+import { CommonService } from 'src/app/services/common.service';
+import { Router } from '@angular/router';
+import { EmailConfigService } from 'src/app/services/email-config.service';
 
 @Component({
   selector: 'app-email-config',
@@ -7,9 +12,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailConfigComponent implements OnInit {
 
-  constructor() { }
+  emailConfig: any[];
+  selectedSparkConfig: any;
+  emailConfigColumns: any;
+  appState: any;
+
+  constructor(
+    private emailConfigService: EmailConfigService,
+    private messageService: MessageService,
+    private commonService: CommonService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.appState = this.commonService.getState();
+    this.emailConfigColumns = [
+      {
+        header: 'EMAIL_GROUP',
+        field: 'EMAIL_GROUP'
+      },
+      {
+        header: 'EMAIL_ADDRESSES',
+        field: 'EMAIL_ADDRESSES'
+      },
+      {
+        header: 'UPDATE_DATE',
+        field: 'UPDATE_DATE',
+        type: 'date'
+      },
+      {
+        header: 'UPDATED_BY',
+        field: 'UPDATED_BY',
+      }
+    ];
+
+    this.loadSparkConfig();
+  }
+
+  loadSparkConfig() {
+    this.emailConfigService.getSparkConfig().subscribe((data: any) => {
+      if (data.data && data.data.length > 0) {
+        this.emailConfig = data.data;
+      }
+    }, error => {
+      this.showToast('error', 'Error while fetching data.');
+    });
+  }
+
+  showToast(severity, summary) {
+    this.messageService.add({ severity, summary, life: 3000 });
+  }
+
+  selectSparkConfig(emailConfig: any, edit: boolean) {
+    this.appState = { ...this.appState, selectedSparkConfig: emailConfig };
+    this.commonService.setState(this.appState);
+    if (edit) {
+      this.router.navigate(['email-config/edit']);
+    } else {
+      this.router.navigate(['email-config/view']);
+    }
   }
 
 }

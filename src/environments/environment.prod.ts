594442b59ea20ffa59f64ebe5eaa30b823993826
Yogesh Rathod <yogesh@mactores.com>
2019-10-22 16:27:23 +0530

    Prod Config Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   baseUrl: 'https://ehcload.seagate.com/api/v1/',
   RAW_FACTORY_PATH_DEFAULT_SETTING: 'stx-usw2-ehc-prd-data-factory',
   T1_PATH_DEFAULT_SETTING: 'stx-usw2-ehc-prd-data-t1',
-  appModulesList: ['Dashboard', 'Load Control', 'Load Status', 'Column Metadata', 'Header Hash', 'User Management', 'DB Endponits', 'Clusters', 'Spark Config'],
+  appModulesList: ['Dashboard', 'Load Control', 'Load Status', 'Column Metadata', 'Header Hash', 'User Management', 'DB Endponits', 'Clusters', 'Spark Config', 'Email Config'],
   defaultPermissions: ['Read', 'Add', 'Edit', 'Delete'],
   ssoLoginURL: 'https://ssotstlogin.seagate.com/oamfed/idp/initiatesso?providerid=SeagateAtlasManager',
   // ssoLogoutURL: 'https://ssotstlogin.seagate.com/oam/server/logout'
